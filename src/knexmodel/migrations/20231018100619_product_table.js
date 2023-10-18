fc123fd3ebb9a279ba60fcf4883d80101750ebd9
Alvenio Farhan Prayogo <alveniofarhan@gmail.com>
2023-10-18 17:18:48 +0700

    update databases

---
+++ src/knexmodel/migrations/20231018100619_product_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("products", (table) => {
+    table.increments("id").primary();
+    table.string("title");
+    table.string("description");
+    table.string("price");
+    table.string("brand");
+    table.string("category");
+    table.string("thumbnail");
+    table.timestamps();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("products");
+};
